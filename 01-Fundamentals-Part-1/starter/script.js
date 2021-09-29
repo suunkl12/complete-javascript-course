c8ab98b8fe5031821300ee01d940385fffa24b4d
suunkl12 <40013740+suunkl12@users.noreply.github.com>
2021-09-29 22:30:18 +0700

    Complete 2 lecture
    1. Value
    2. Variables and Data types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,37 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let $function = 27;
+
+
+
+*/
+
+let javascripIsFun = true;
+
+console.log(typeof javascripIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascripIsFun = "YES!";
+console.log(typeof javascripIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
